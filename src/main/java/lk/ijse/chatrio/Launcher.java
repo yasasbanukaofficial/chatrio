aae7e097edd6df9fc5b72de6e824d9e269a4e4e8
Yasas Banuka <yasasbanukaofficial@gmail.com>
2025-10-13 19:59:51 +0530

    feat(ui): adjusted ui sizes and padding

--- src/main/java/lk/ijse/chatrio/Launcher.java
+++ src/main/java/lk/ijse/chatrio/Launcher.java
@@ -15,17 +15,17 @@ public class Launcher extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent clientParent = FXMLLoader.load(Objects.requireNonNull(Launcher.class.getResource("/view/Client.fxml")));
+        Parent clientParent = FXMLLoader.load(Objects.requireNonNull(Launcher.class.getResource("/view/Server.fxml")));
         Scene scene = new Scene(clientParent);
         stage = new Stage();
         stage.setScene(scene);
         stage.show();
 
-        Parent serverParent = FXMLLoader.load(Objects.requireNonNull(Launcher.class.getResource("/view/Server.fxml")));
+        Parent serverParent = FXMLLoader.load(Objects.requireNonNull(Launcher.class.getResource("/view/Client.fxml")));
         Scene serverScene = new Scene(serverParent);
         Stage serverStage = new Stage();
-        serverStage.setWidth(stage.getWidth());
-        serverStage.setHeight(stage.getHeight());
+        serverStage.setWidth(stage.getWidth() + 10);
+        serverStage.setHeight(stage.getHeight() + 30);
         serverStage.setScene(serverScene);
         serverStage.show();
     }
